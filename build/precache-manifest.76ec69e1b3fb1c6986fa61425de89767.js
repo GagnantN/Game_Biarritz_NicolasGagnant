e93837cb56fea5b751a77ba078fcc61899900616
wilder <gagnantnicolas64@gmail.com>
2019-07-25 16:42:36 +0200

    netlify deploy 2

---
+++ build/precache-manifest.76ec69e1b3fb1c6986fa61425de89767.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e4b5ae90260254773041bde7d60ccf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60244dcefb937b98ba83",
+    "url": "/static/css/main.ee11abea.chunk.css"
+  },
+  {
+    "revision": "27d07c07d8641218cbde",
+    "url": "/static/js/2.143b0b4d.chunk.js"
+  },
+  {
+    "revision": "60244dcefb937b98ba83",
+    "url": "/static/js/main.88ad7cb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
